965b8231ce2b9089b8179e66c9667dcd1871fcc2
clauga <guerrero.ayala.cs@gmail.com>
2023-10-09 16:57:03 +0200

    quest 02 spring-boot

--- src/main/java/com/wildcodeschool/doctor/DoctorWhoApplication.java
+++ src/main/java/com/wildcodeschool/doctor/DoctorWhoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DoctorWhoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DoctorWhoApplication.class, args);
 	}
 
--- src/main/java/com/wildcodeschool/doctor/model/Doctor.java
+++ src/main/java/com/wildcodeschool/doctor/model/Doctor.java
@@ -14,14 +14,17 @@ public class Doctor {
     }
 
     public int getNumber() {
+
         return number;
     }
 
     public void setNumber(int number) {
+
         this.number = number;
     }
 
     public String getName() {
+
         return name;
     }
 
